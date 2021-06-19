a0268d0c4b2d201777d6f2c23953bd1a2d99daec
aymansohag <aymansohag.contact@gmail.com>
2021-06-19 19:23:23 +0600

    laravel setup

---
+++ resources/views/Admin/index.blade.php
@@ -0,0 +1,50 @@
+@extends('Admin.app')
+
+
+@section('content')
+
+<div class="nk-block-head nk-block-head-sm">
+    <div class="nk-block-between">
+        <div class="nk-block-head-content">
+            <h3 class="nk-block-title page-title">Sales Overview</h3>
+            <div class="nk-block-des text-soft">
+                <p>Welcome to DashLite Dashboard Template.</p>
+            </div>
+        </div><!-- .nk-block-head-content -->
+        <div class="nk-block-head-content">
+            <div class="toggle-wrap nk-block-tools-toggle">
+                <a href="#" class="btn btn-icon btn-trigger toggle-expand mr-n1" data-target="pageMenu"><em class="icon ni ni-more-v"></em></a>
+                <div class="toggle-expand-content" data-content="pageMenu">
+                    <ul class="nk-block-tools g-3">
+                        <li>
+                            <div class="dropdown">
+                                <a href="#" class="dropdown-toggle btn btn-white btn-dim btn-outline-light" data-toggle="dropdown"><em class="d-none d-sm-inline icon ni ni-calender-date"></em><span><span class="d-none d-md-inline">Last</span> 30 Days</span><em class="dd-indc icon ni ni-chevron-right"></em></a>
+                                <div class="dropdown-menu dropdown-menu-right">
+                                    <ul class="link-list-opt no-bdr">
+                                        <li><a href="#"><span>Last 30 Days</span></a></li>
+                                        <li><a href="#"><span>Last 6 Months</span></a></li>
+                                        <li><a href="#"><span>Last 1 Years</span></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="nk-block-tools-opt"><a href="#" class="btn btn-primary"><em class="icon ni ni-reports"></em><span>Reports</span></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div><!-- .nk-block-head-content -->
+    </div><!-- .nk-block-between -->
+</div><!-- .nk-block-head -->
+<div class="nk-block">
+    <div class="row g-gs">
+        <div class="col-xxl-6">
+            <div class="card card-bordered h-100">
+                <div class="card-inner">
+
+                </div>
+            </div><!-- .card -->
+        </div><!-- .col -->
+    </div><!-- .row -->
+</div><!-- .nk-block -->
+
+@endsection
